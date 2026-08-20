c81ea50c5ec7f2f6ac8c66e37b4088b4ad2d10f2
ndindootttttt <dia.ayu2976@smk.belajar.id>
2026-08-20 13:40:58 +0700

    update halaman login

--- jelajahpo-frontend/src/pages/Login.jsx
+++ jelajahpo-frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
    const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5000/login", {
+      const res = await fetch("http://localhost:4000/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
