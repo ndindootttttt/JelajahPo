48e58cc03edaef01bf3d4140d7677331c9276cc2
ndindootttttt <dia.ayu2976@smk.belajar.id>
2026-08-20 10:23:41 +0700

    salah port

--- jelajahpo-frontend/src/pages/Wisata.jsx
+++ jelajahpo-frontend/src/pages/Wisata.jsx
@@ -6,7 +6,7 @@ export default function Wisata() {
  
   const getWisata = async () => {
     try {
-      const res = await fetch("http://localhost:5000/wisata");
+      const res = await fetch("http://localhost:3001/wisata");
       const data = await res.json();
       setWisata(data);
     } catch (err) {
