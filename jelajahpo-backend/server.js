e5281a2c614f9101ce0b583797f53eb8161e5a5d
ndindootttttt <dia.ayu2976@smk.belajar.id>
2026-08-20 13:03:12 +0700

    editwisata.jsx

--- jelajahpo-backend/server.js
+++ jelajahpo-backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = 4000;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const mysql = require('mysql2'); 
 
 const db = mysql.createConnection({
@@ -29,6 +31,15 @@ app.get('/', (req, res) => {
 app.get('/wisata', (req, res) => {
   const sql = 'SELECT * FROM wisata';
   db.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: err });
+    res.json(results);
+  });
+});
+
+app.get('/wisata/:id_wisata', (req, res) => {
+  const { id_wisata } = req.params;
+  const sql = 'SELECT * FROM wisata WHERE id_wisata = ?';
+  db.query(sql, [id_wisata], (err, results) => {
     if (err) return res.status(500).json({ error: err });
     res.json(results);
   });
@@ -92,6 +103,36 @@ app.get('/kategori', (req, res) => {
     res.json(results);
   });
 });
+
+app.post('/pengguna', async (req, res) => {
+  const { nama, email, password, no_hp } = req.body;
+
+  if (!nama || !email || !password) {
+    return res.status(400).json({ message: 'Nama, email, dan password wajib diisi' });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const sql = 'INSERT INTO pengguna (nama, email, password, no_hp) VALUES (?, ?, ?, ?)';
+
+    db.query(sql, [nama, email, hashedPassword, no_hp], (err, result) => {
+      if (err) {
+          if (err.code === 'ER_DUP_ENTRY') {
+          return res.status(400).json({ message: 'Email sudah terdaftar, gunakan email lain' });
+        }
+        return res.status(500).json({ error: err.sqlMessage });
+      }
+
+      res.json({
+        message: 'Akun berhasil dibuat!',
+        id_pengguna: result.insertId
+      });
+    });
+  } catch (err) {
+    res.status(500).json({ error: 'Gagal mengenkripsi password' });
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server JelajahPo jalan di http://localhost:${PORT}`);
--- jelajahpo-frontend/src/pages/Wisata.jsx
+++ jelajahpo-frontend/src/pages/Wisata.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState,  } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
  
  
 export default function Wisata() {
   const [wisata, setWisata] = useState([]);
   const [loading, setLoading] = useState(true);
+   const navigate = useNavigate();
  
   const getWisata = async () => {
     try {
@@ -21,6 +22,29 @@ export default function Wisata() {
   useEffect(() => {
     getWisata();
   }, [])
+
+const handleDelete = async (id) => {
+  if (window.confirm("Yakin ingin menghapus wisata ini?")) {
+    try {
+      const res = await fetch(`http://localhost:5000/wisata/${id}`, {
+        method: "DELETE",
+      });
+      if (res.ok) {
+        alert("Wisata berhasil dihapus");
+        getWisata(); // ambil ulang data terbaru
+      } else {
+        alert("Gagal menghapus wisata");
+      }
+    } catch (err) {
+      console.error("Error saat delete:", err);
+      alert("Terjadi kesalahan saat menghapus data");
+    }
+  }
+};
+ 
+const handleEdit = (id) => {
+  navigate(`/wisata/edit/${id}`);
+};
 
     if (loading) {
     return <div className="container mt-4">Sedang memuat data...</div>;
@@ -41,6 +65,8 @@ export default function Wisata() {
             <th>Nama Wisata</th>
             <th>Deskripsi</th>
             <th>Harga Tiket</th>
+            <th>Edit</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
@@ -51,6 +77,23 @@ export default function Wisata() {
                 <td>{item.nama_wisata}</td>
                 <td>{item.deskripsi}</td>
                 <td>Rp {item.harga_tiket}</td>
+                <td>
+  <button
+    className="btn btn-warning btn-sm me-2"
+    onClick={() => handleEdit(item.id_wisata)}
+  >
+    Edit
+  </button>
+  </td>
+  <td>
+  <button
+    className="btn btn-danger btn-sm"
+    onClick={() => handleDelete(item.id_wisata)}
+  >
+    Delete
+  </button>
+</td>
+
               </tr>
             ))
           ) : (
